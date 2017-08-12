64f32c4a3adf0a8723e8debed6ab71164dbd7147
Treinamento <treinamento@Treinamento1-Maquina21.local>
2017-08-12 11:49:30 -0300

    Carregando Collection view com Array

---
+++ CollectionViewProject/CollectionViewProject/TesteCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  TesteCollectionViewCell.m
+//  CollectionViewProject
+//
+//  Created by Treinamento on 12/08/17.
+//  Copyright © 2017 Ribamar. All rights reserved.
+//
+
+#import "TesteCollectionViewCell.h"
+
+@implementation TesteCollectionViewCell
+
+@end
